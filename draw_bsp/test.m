1b7a140ca0f1b389bcb4e5b68e1fe18d94c62d03
bookie <zhangboshu9115@163.com>
2021-05-06 14:53:08 +0300

    draw b spline from control point

---
+++ draw_bsp/test.m
@@ -0,0 +1,22 @@
+clear;
+close all;
+
+hold on;
+grid on;
+
+%% init control point
+control_point =[1,2;2,3;3,1;5,2;4,-1];
+plot(control_point(:,1), control_point(:,2));
+
+k = 3;
+n = 4;
+knots = [zeros(1,k-1) linspace(0, 1, n-k+3) ones(1,k-1)];
+
+m = 1000; 
+t = linspace(0, 1, m);
+X = ones(m, 2);
+for i = 1 : m  
+    X(i,:) = CoxdeBoor(t(i), control_point, knots, k);
+end
+
+plot(X(:,1), X(:,2));
