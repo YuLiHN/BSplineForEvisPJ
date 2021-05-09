08d7192c575c5c3e462fd8c9199f60abda1c6d0c
bookie <zhangboshu9115@163.com>
2021-05-09 17:03:33 +0300

    fix

--- Matlab/draw_bsp/reverse_bsp.m
+++ Matlab/draw_bsp/reverse_bsp.m
@@ -68,18 +68,19 @@ plot(control_point(:,1),control_point(:,2),'b');
 plot(control_point(:,1),control_point(:,2),'b*'); 
 hold on
 
-
-x=0;y=0;down=0;
+mm = 0;
+m = 101;
+X = ones(m, 2);
 
 for j=1:(n-1)
-    uu=(knots(j+3)):0.005:knots(j+4);
+    uu=(knots(j+3)):0.01:knots(j+4);
     for kk=1:length(uu)
-       down=down+1;
-        x(down)=control_point(j,1)*RCoxdeBoor(j,3,knots,uu(kk))+control_point(j+1,1)*RCoxdeBoor(j+1,3,knots,uu(kk))+control_point(j+2,1)*RCoxdeBoor(j+2,3,knots,uu(kk))+control_point(j+3,1)*RCoxdeBoor(j+3,3,knots,uu(kk));
-        y(down)=control_point(j,2)*RCoxdeBoor(j,3,knots,uu(kk))+control_point(j+1,2)*RCoxdeBoor(j+1,3,knots,uu(kk))+control_point(j+2,2)*RCoxdeBoor(j+2,3,knots,uu(kk))+control_point(j+3,2)*RCoxdeBoor(j+3,3,knots,uu(kk));
+        mm=mm+1;
+        X(mm,1)=control_point(j,1)*RCoxdeBoor(j,3,knots,uu(kk))+control_point(j+1,1)*RCoxdeBoor(j+1,3,knots,uu(kk))+control_point(j+2,1)*RCoxdeBoor(j+2,3,knots,uu(kk))+control_point(j+3,1)*RCoxdeBoor(j+3,3,knots,uu(kk));
+        X(mm,2)=control_point(j,2)*RCoxdeBoor(j,3,knots,uu(kk))+control_point(j+1,2)*RCoxdeBoor(j+1,3,knots,uu(kk))+control_point(j+2,2)*RCoxdeBoor(j+2,3,knots,uu(kk))+control_point(j+3,2)*RCoxdeBoor(j+3,3,knots,uu(kk));
     end
 end
 
-plot(x,y,'red');
+plot(X(:,1),X(:,2),'red');
  
 xlabel('x');ylabel('y');
